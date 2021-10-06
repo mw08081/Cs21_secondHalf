b060163283f4c3d19540eea4343eec2dd046fa69
mw304 <kksmiss34@gmail.com>
2021-10-06 22:25:24 +0900

    add struct extensionMethod & add double.sqrt()(The Babylonian Method)

---
+++ ExtensionMethod/ClassExtensionMethod/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+using static System.Console;
+
+namespace ExtensionMethod
+{
+    public static class intExtension
+    {
+        public static int Pow(this int powValue, int powCnt)
+        {
+            int tmp = 1;
+
+            for (int i = 0; i < powCnt; i++)
+                tmp *= powValue;
+
+            return tmp;
+        }
+    }
+
+    public static class doubleExtension
+    {
+        public static double Pow(this double powValue, int powCnt)
+        {
+            double tmp = 1;
+
+            for (int i = 0; i < powCnt; i++)
+                tmp *= powValue;
+
+            return tmp;
+        }
+
+        public static double Sqrt(this double sqrtValue)
+        {
+            double x = 2;
+
+            for (int i = 0; i < 10; i++)
+                x = (x + (sqrtValue / x)) / 2;
+
+            return x;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine($"int.pow(3,3) = {3.Pow(3)}");
+            WriteLine($"int.pow(5,3) = {5.Pow(3)}");
+
+            WriteLine($"double.pow(2.5,2) = {2.5.Pow(2)}");
+            WriteLine($"double.sqrt(2) = {2.0.Sqrt()}");
+        }
+    }
+}
---
+++ ExtensionMethod/StructExtensionMethod/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using static System.Console;
+
+namespace StructExtensionMethod
+{
+    public struct point                                         //if u dont assign public, ErrorMSG occur because of difficulty reference to Struct point
+    {
+        public double x;
+        public double y;
+    }
+
+    public static class PointExtension
+    {
+        public static double PrintDistance(this point pt) 
+        {
+            return (pt.x.Pow(2) + pt.y.Pow(2)).Sqrt();
+        }
+    }
+
+    public static class doubleExtension
+    {
+        public static double Pow(this double powValue, int powCnt)
+        {
+            double tmp = 1;
+
+            for (int i = 0; i < powCnt; i++)
+                tmp *= powValue;
+
+            return tmp;
+        }
+
+        public static double Sqrt(this double sqrtValue)
+        {
+            double x = 2;
+
+            for (int i = 0; i < 10; i++)
+                x = (x + (sqrtValue / x)) / 2;
+
+            return x;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            point pt = new point();
+            pt.x = 3.0;
+            pt.y = 4.0;
+
+            WriteLine($"x : {pt.x}, y : {pt.y}");
+            WriteLine($"O to pt distance : {pt.PrintDistance()}");
+
+
+        }
+    }
+}
